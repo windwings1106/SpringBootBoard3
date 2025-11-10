6a7912c1c4da6d0e98dedf553b68bd931cc8ce9a
windwings1106 <windwings1106@gmail.com>
2025-11-10 11:09:37 +0900

    2-05

---
+++ sbb/src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -0,0 +1,12 @@
+package com.mysite.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface QuestionRepository extends JpaRepository<Question, Integer>{
+	Question findBySubject(String subejct);
+	Question findBySubjectAndContent(String subject, String content);
+	List<Question> findBysubjectLike(String subject);
+
+}
--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,34 @@
 package com.mysite.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbbApplicationTests {
+	
+	@Autowired
+	private QuestionRepository questionRepository;
+	
+	@Autowired
+	private AnswerRepository answerRepository;
 
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		
+		Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(q);
+		a.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(a);
 	}
 
 }
